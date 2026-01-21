3cb2f278c3f408eca5330fd44018be6b13f145b6
Blackrose-blackhat <musharafz2k3@gmail.com>
2026-01-22 00:33:11 +0530

    ngrok

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://e494cbc4d082.ngrok-free.app/api';
 
 const api = axios.create({
     baseURL: API_URL,
